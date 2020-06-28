888cac5515e4461727757e23410bf38891f84831
DieVlogger <67368155+DieVlogger@users.noreply.github.com>
2020-06-28 16:22:00 +0200

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'ping',
+    description: 'Ping!',
+    cooldown: 5,
+    execute(message) {
+        // teamBOT BOTCOMMANDS TESTBOTCOMMANDS
+        message.channel.send('Pong.');
+    },
+};
